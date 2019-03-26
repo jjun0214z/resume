60e85b45abf2489de622c65855af5f8121b1c99e
jjun0214z <jjun0214z@gmail.com>
2019-03-26 19:47:47 +0900

    Updates

---
+++ precache-manifest.cb4744bd28801d530e11a52433714f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18e0f9fbb9f0cc9c4f961c2aa7efce06",
+    "url": "/resume/static/media/min_photo.18e0f9fb.jpg"
+  },
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.32a5f7b9.js"
+  },
+  {
+    "revision": "743f7a570b6945023b06",
+    "url": "/resume/static/js/main.c6ed5754.chunk.js"
+  },
+  {
+    "revision": "8d4ce3a0f3a7d7880857",
+    "url": "/resume/static/js/2.0c624ee0.chunk.js"
+  },
+  {
+    "revision": "743f7a570b6945023b06",
+    "url": "/resume/static/css/main.b1174232.chunk.css"
+  },
+  {
+    "revision": "3bb61b12d59d655557589add05625257",
+    "url": "/resume/index.html"
+  }
+];
